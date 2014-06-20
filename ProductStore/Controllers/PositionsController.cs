d5f59ff617a21a92953ba713b0206a14d1c5ba76
Martin Mihura <mbmihura@gmail.com>
2014-06-20 16:42:11 -0300

    Position table list user with equal score in same row.

--- ProductStore/Controllers/PositionsController.cs
+++ ProductStore/Controllers/PositionsController.cs
@@ -13,7 +13,19 @@ namespace Prode.Controllers
         // GET api/positions/
         public List<Position> GetAll()
         {
-            return Position.GetTable();
-        }
+            return Position.GetTable()
+                .GroupBy(p => p.Points)
+                .Select(p => new Position { 
+                    Points = p.Key,
+                    User = p.Aggregate(String.Empty, 
+                        (s, p2) => s + (string.IsNullOrEmpty(s) ?"":", ") + p2.User
+                    )
+                }).ToList<Position>();
+           }
+
+
+
+
+
     }
 }
